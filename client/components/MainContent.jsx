27a44b8e0e1e0ba00f03d95d21fbc2dde74a966c
mrmgreen <m.r.green@hotmail.co.uk>
2017-01-27 18:09:20 +0000

    added mousemove - image moves with

--- client/components/MainContent.jsx
+++ client/components/MainContent.jsx
@@ -4,8 +4,6 @@ import Meta from './meta';
 class MainContent extends React.Component {
   constructor(props) {
     super(props);
-    // this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.handleMouseUp = this.handleMouseUp.bind(this);
     this.state = {
       onMouseMove: false,
       position: {
@@ -19,7 +17,6 @@ class MainContent extends React.Component {
     let y;
     if (this.imageNode) {
       const dimensions = this.imageNode.getBoundingClientRect();
-      console.log('dims = ', dimensions)
       x = dimensions.width/2;
       y = dimensions.height/2
     }
@@ -41,21 +38,6 @@ class MainContent extends React.Component {
     }
   }
 
-  // handleMouseMove (e) {
-  //   const imageBounds = e.target.getBoundingClientRect();
-  //   const imageLeft = imageBounds.left;
-  //   const imageTop = imageBounds.top;
-  //   const xClick = e.clientX - imageLeft;
-  //   const yClick = e.clientY - imageTop;
-  //   const moveTo = xClick;
-  //   console.log('xClick and y', xClick, yClick);
-  //   this.setState({ imageLeft: moveTo });
-  // }
-
-  handleMouseUp (e) {
-    this.setState({ onMouseMove: false });
-  }
-
   render() {
 
     return (
@@ -63,8 +45,6 @@ class MainContent extends React.Component {
         <h2>Main content</h2>
         <div className="image-container"
           onClick={this.handleOnClick}
-          onMouseMove={this.state.onMouseMove ? this.handleMouseMove : null}
-          onMouseUp={this.handleMouseUp}
           style={{ position: "absolute", left: `${this.state.position.left}px`, top: `${this.state.position.top}px` }}
         >
           <img src={this.props.image} draggable="false" ref={node => this.imageNode = node} />
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -17,9 +17,12 @@ class App extends React.Component {
       bio: null,
       occupation: null,
       position: { left: 500, top: 200 },
+      onMouseMove: false,
     }
     this.logMeIn = this.logMeIn.bind(this);
     this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
   }
 
   fetchData(person) {
@@ -60,7 +63,15 @@ class App extends React.Component {
   handleMouseDown (e) {
     const xClick = e.clientX;
     const yClick = e.clientY;
-    this.setState({ position: { left: e.clientX, top: e.clientY }});
+    this.setState({ position: { left: e.clientX, top: e.clientY }, onMouseMove: true });
+  }
+
+  handleMouseMove (e) {
+    this.setState({ position: { left: e.clientX, top: e.clientY } });
+  }
+
+  handleMouseUp (e) {
+    this.setState({ onMouseMove: false });
   }
 
   render() {
@@ -76,7 +87,11 @@ class App extends React.Component {
     />;
     let content = this.state.loggedIn === false ? mainContent : login;
     return (
-      <div className="iphone" onMouseDown={this.handleMouseDown} >
+      <div className="iphone"
+        onMouseDown={this.handleMouseDown}
+        onMouseMove={this.state.onMouseMove ? this.handleMouseMove : null}
+        onMouseUp={this.handleMouseUp}
+      >
         <div className="content-container">
           <Header />
           {content}
